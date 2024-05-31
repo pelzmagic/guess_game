9e2b8f3b9b7a8e1e98770e2133e0b90486cefb38
pelzmagic <akinloyepelz@gmail.com>
2024-05-31 22:41:02 +0100

    a guess game

--- index.js
+++ index.js
@@ -10,11 +10,41 @@ form.addEventListener('submit', (e) => {
     const userInput = inputDetails.value;
     console.log(userInput);
     // Check if the user tries to submit an empty input
-
-    if (userInput === '') {
+     if (userInput === '') {
         inputDetails.classList.add("error");
         errorImage.classList.remove("hide");
     } else {
         inputDetails.classList.remove("error");
+    }
+
+
+      // Validate user input to stop them from entering a string or a decimal
+      if (!userInput.match(/^[0-9]*$/)) {
+        result.innerHTML = "Please, enter a number, omo weyrey.";
+        result.style.color = "red";
+        return false;
+    } else if (userInput.match(/^[0-9]*$/)) {
+        result.innerHTML = "";
+    }
+
+    // Checks if the user inputs a number that is too low or close
+    if (userInput <= 50) {
+        result.innerHTML = "Your guess is too low! You have two more tries.";
+        result.style.color = "red";
+    } else if (userInput > 50 && userInput < 88) {
+        result.innerHTML = "Your guess is pretty close, you're almost there!";
+        result.style.color = "green";
+    }
+
+    // Checks if the user inputs the exact number or a number higher than the exact random number
+    if (userInput == 88) {
+        result.innerHTML = "Your head is there! That's the exact number i picked.";
+        result.style.color = "green";
+    } else if (userInput > 88 && userInput <= 100) {
+        result.innerHTML = " Ahnahn, The number you have entered is too high, calm down.";
+        result.style.color = "black";
+    } else if (userInput > 100) {
+        result.innerHTML = "Please, enter a number between 1 - 100, olodo!";
+        result.style.color = "red";
     }
 })
